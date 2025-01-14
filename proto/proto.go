1b98daf9acdea32398ac569d82cfbc8c5dca2384
teamcutter <ruslan.sibgatulin05@gmail.com>
2025-01-14 18:30:42 +0500

    added json response

--- proto/proto.go
+++ proto/proto.go
@@ -1,6 +1,7 @@
 package proto
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -23,7 +24,12 @@ func (c *CommandSET) Execute(peer *peers.Peer) error {
 	log.Printf("Executing SET command: key=%s, value=%s", c.Key, c.Value)
 
 	peer.Set(c.Key, c.Value)
-	response := fmt.Sprintf("SET OK: %s = %s\n", c.Key, c.Value)
+
+	response, err := formatResponse(c.String(), c.Key, c.Value)
+	if err != nil {
+		return err
+	}
+
 	return peer.Send(response)
 }
 
@@ -42,7 +48,12 @@ func (c *CommandGET) Execute(peer *peers.Peer) error {
 	if err != nil {
 		return err
 	}
-	response := fmt.Sprintf("VALUE: %s\n", val)
+
+	response, err := formatResponse(c.String(), c.Key, val)
+	if err != nil {
+		return err
+	}
+
 	return peer.Send(response)
 }
 
@@ -61,7 +72,12 @@ func (c *CommandDEL) Execute(peer *peers.Peer) error {
 	if err != nil {
 		return err
 	}
-	response := fmt.Sprintf("DEL OK: %s = %s\n", c.Key, val)
+
+	response, err := formatResponse(c.String(), c.Key, val)
+	if err != nil {
+		return err
+	}
+
 	return peer.Send(response)
 }
 
@@ -95,6 +111,21 @@ func parseCommand(msg string) (Command, error) {
 	default:
 		return nil, errors.New("unknown command")
 	}
+}
+
+func formatResponse(command, key, value string) (string, error) {
+	responseData := map[string]string{
+		"status": command + " OK",
+		"key":    key,
+		"value":  value,
+	}
+
+	prettyJSON, err := json.MarshalIndent(responseData, "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("error marshalling to JSON: %v", err)
+	}
+
+	return string(prettyJSON), nil
 }
 
 func HandleCommand(msg string, peer *peers.Peer) error {
